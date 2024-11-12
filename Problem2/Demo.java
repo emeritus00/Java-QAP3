8249f431b47466b036cce9d407bedc5537996906
Adewale Gbadamosi <adewalegbadamosi@gmail.com>
2024-11-12 19:02:54 -0330

    First commit

---
+++ Problem2/Demo.java
@@ -0,0 +1,18 @@
+package Problem2;
+
+// Demo.java
+public class Demo {
+    public static void main(String[] args) {
+        // Creating Point instance
+        Point point = new Point(2.5f, 3.5f);
+        System.out.println("\nPoint: " + point);
+
+        // Creating MovablePoint instance
+        MovablePoint movablePoint = new MovablePoint(2.5f, 3.5f, 1.0f, 1.5f);
+        System.out.println("MovablePoint before move: " + movablePoint);
+
+        // Moving the MovablePoint
+        movablePoint.move();
+        System.out.println("MovablePoint after move: " + movablePoint + "\n");
+    }
+}
